69f59edf54c3b53bb801570101d524f4fe7a9943
Javier Perez <javier.alejandro92@gmail.com>
2015-03-11 17:54:28 -0600

    enhanced examples on messages

--- examples/messages.php
+++ examples/messages.php
@@ -2,13 +2,24 @@
 date_default_timezone_set("America/Guatemala");
 include("../src/SmsApi.php");
 
-$api = new SmsApi("n90GovNeANyXaZWFkGC3TRhVAByRxrVg", "UE3BY99Kk2BUnfqJAq8YqqNnyy5pZ86Q", "http://localhost:8088/api");
+$api = new SmsApi("n90GovNeANyXaZWFkGC3TRhVAByRxrVg", "UE3BY99Kk2BUnfqJAq8YqqNnyy5pZ86Q", "http://localhost:8088/api", false);
 
 print ("Getting messages...");
-var_dump($api->messages()->getMessages("2015-03-01", "2015-03-10",2));
+$response = $api->messages()->getMessages("2015-03-01", "2015-03-10",2);
+if ($response->status == "OK"){
+    foreach ($response->data as $message) echo $message->message."\n";
+}
+else echo "Failed to get messages with status code $response->code\n";
+
 
 print ("Sending message to contact...");
-var_dump($api->messages()->sendToContact("50212345678", "Sent from PHP SDK"));
+$resposne = $api->messages()->sendToContact("50212345678", "Sent from PHP SDK");
+if ($response->status == "OK") echo "I sent a message!\n";
+else echo "Failed to send message with status code $response->code\n";
+
 
 print ("Sending message to group...");
-var_dump($api->messages()->sendToGroups(array("test"), "Test message to group"));
+$response = $api->messages()->sendToGroups(array("test"), "Test message to group");
+if ($response->status == "OK") echo "I sent a message to the test group!";
+else echo "Failed to send a message to group with status code $response->code\n";
+
